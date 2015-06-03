3114a34fc5289d96884e578872ec1ccfc0a9812f
Clarice <clasr7@gmail.com>
2015-06-03 19:23:58 -0300

    player sobe ao apertar UP

--- Game-C/main.c
+++ Game-C/main.c
@@ -14,44 +14,136 @@
 //GLOBALS
 const int WIDTH = 1200; //largura display
 const int HEIGHT = 500; //altura display
+enum KEYS{UP, Q, W, E};
+bool keys[5] = {false, false, false, false};
 
 //prototypes
 void InitPlayer(Player &player);
 void DrawPlayer(Player &player);
+void PlayerJump(Player &player);
+//void PlayerShootQ(Player &player);
+//void PlayerShootW(Player &player);
+//void PlayerShootE(Player &player);
 
 //main
 int main()
 {
     //primitive variables
+    const int FPS = 60;
     bool done = false;
+    bool redraw = true;
 
     //object variables
     Player player;
 
     //allegro variables
     ALLEGRO_DISPLAY *display;
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+    ALLEGRO_TIMER *timer = NULL;
+
+////////////////////////////////////////////////////////////////////////
 
     //initializate functions
     if(!al_init())
             return -1; //erro caso inicializar
 
-        display = al_create_display(WIDTH,HEIGHT); //criar display
+    display = al_create_display(WIDTH,HEIGHT); //criar display
 
-        if(!display)
-            return -1; //se der merda
+    if(!display)
+        return -1; //se der merda
 
-        al_init_primitives_addon();
-        InitPlayer(player); //chamar função que "inicia" player
+    //Allegro Module Init
+    al_init_primitives_addon();
+    al_install_keyboard();
 
-        while (!done) {
-            DrawPlayer(player); //desenha player
-            al_flip_display(); //atualiza a tela
-            al_clear_to_color(al_map_rgb(0,0,0)); //tela preta
-        }
+    event_queue = al_create_event_queue();
+    timer = al_create_timer(1.0 / FPS);
 
-        al_rest(5.0f);
+    //Game Init
+    InitPlayer(player); //chamar função que "inicia" player
 
-        al_destroy_display(display);
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+    al_start_timer(timer);
+
+    while (!done) {
+        ALLEGRO_EVENT ev;
+    	al_wait_for_event(event_queue, &ev);
+
+        //se clicar para fechar a janela
+        if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+    	{
+    		done = true;
+    	}
+        else if(ev.type == ALLEGRO_EVENT_TIMER)
+		{
+            redraw = true;
+            if(keys[UP])
+    		PlayerJump(player);
+    		//if(keys[Q])
+    		      //PlayerShootQ
+    		//if(keys[W])
+    		      //PlayerShootW
+    		//if(keys[E])
+    			//PlayerShootE
+    	}
+
+        else if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
+    	{
+    		switch(ev.keyboard.keycode)
+    		{
+    			case ALLEGRO_KEY_ESCAPE:
+    				done = true;
+    				break;
+    			case ALLEGRO_KEY_UP:
+    				keys[UP] = true;
+    				break;
+    			/*case ALLEGRO_KEY_Q:
+    				keys[Q] = true;
+    				break;
+    			case ALLEGRO_KEY_W:
+    				keys[Q] = true;
+    				break;
+    			case ALLEGRO_KEY_E:
+    				keys[E] = true;
+    				break;*/
+
+    		}
+    	}
+
+        else if(ev.type == ALLEGRO_EVENT_KEY_UP)
+    	{
+    		switch(ev.keyboard.keycode)
+    		{
+    			case ALLEGRO_KEY_UP:
+    				keys[UP] = false;
+    				break;
+    			/*case ALLEGRO_KEY_Q:
+    				keys[Q] = false;
+    				break;
+    			case ALLEGRO_KEY_W:
+    				keys[W] = false;
+    				break;
+    			case ALLEGRO_KEY_E:
+    				keys[E] = false;
+    				break;*/
+			}
+		}
+
+        if(redraw && al_is_event_queue_empty(event_queue))
+		{
+			redraw = false;
+
+			DrawPlayer(player);
+
+			al_flip_display();
+			al_clear_to_color(al_map_rgb(0,0,0));
+		}
+    }
+
+    al_destroy_display(display);
 
     return 0;
 }
@@ -69,4 +161,11 @@ void InitPlayer (Player &player) {
 
 void DrawPlayer(Player &player) {
     al_draw_filled_rectangle(player.x, player.y, player.x + 40, player.y - 70 , al_map_rgb(0, 150, 255));
+}
+
+void PlayerJump(Player &player)
+{
+	player.y -= player.speed;
+	if(player.y < 0)
+		player.y = 0;
 }
