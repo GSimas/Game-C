b69e91d865daea80282f79dfbf6ba84ed0156d6b
Clarice <clasr7@gmail.com>
2015-06-03 16:44:59 -0300

    player (rec) desenhado.

--- Game-C/main.c
+++ Game-C/main.c
@@ -7,11 +7,17 @@
 
 //inclusão de bibliotecas
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_native_dialog.h>
-#include "objects.h"
+#include "objects.h" //arquivo de objetos
+
+//GLOBALS
+const int WIDTH = 1200; //largura display
+const int HEIGHT = 500; //altura display
 
 //prototypes
 void InitPlayer(Player &player);
+void DrawPlayer(Player &player);
 
 //main
 int main()
@@ -29,7 +35,7 @@ int main()
     if(!al_init())
             return -1; //erro caso inicializar
 
-        display = al_create_display(800,600); //criar display
+        display = al_create_display(WIDTH,HEIGHT); //criar display
 
         if(!display)
             return -1; //se der merda
@@ -48,4 +54,19 @@ int main()
         al_destroy_display(display);
 
     return 0;
+}
+
+void InitPlayer (Player &player) {
+    player.ID = PLAYER;
+    player.x = 10;
+    player.y = HEIGHT;
+    player.lives = 3;
+    player.speed = 7;
+    player.bundx = 6;
+    player.boundy = 7;
+    player.score = 0;
+}
+
+void DrawPlayer(Player &player) {
+    al_draw_filled_rectangle(player.x, player.y, player.x + 40, player.y - 70 , al_map_rgb(0, 150, 255));
 }
--- Game-C/objects.h
+++ Game-C/objects.h
@@ -3,12 +3,12 @@ enum IDS{PLAYER, ENEMY, BULLET};
 
 //estrutura do player
 struct Player {
-    int ID; //identidade
+    int ID;
     int x; //posição x
     int y; //posição y
-    int lives; //vidas
-    int speed; //velocidade
+    int lives;
+    int speed;
     int bundx; //(?) descobrir ainda
     int boundy; //(?) descobrir ainda
-    int score; //pontuação do jogador
-}
+    int score;
+};
